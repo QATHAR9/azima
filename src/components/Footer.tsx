baa261d6fe9966b141a8dfe2533ffb3798c211ae
QATHAR9 <158095381+QATHAR9@users.noreply.github.com>
2025-08-20 10:57:02 +0300

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -72,7 +72,7 @@ const Footer = () => {
               </div>
               <div className="flex items-center space-x-3">
                 <Mail className="w-5 h-5 text-emerald-400" />
-                <span className="text-gray-300">info@sifaestate.com</span>
+                <span className="text-gray-300">info@azimapropertiesltd.com</span>
               </div>
             </div>
           </div>
